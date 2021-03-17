60f30eadc3d2781180856050320590f48abe882a
fahmibakar <55430230+fahmibakar@users.noreply.github.com>
2021-03-17 17:19:32 +0800

    add folder for document and run macro using python

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
+import os, os.path
+import win32com.client
 from flask import Flask, render_template
+
+if os.path.exists("DGS-200078_NIISe_Master_Implementation_Plan_Projek_NIISe_20210117_0802_v0.3.docx"):
+    file = "DGS-200078_NIISe_Master_Implementation_Plan_Projek_NIISe_20210117_0802_v0.3.docx"
+    xl=win32com.client.Dispatch("Word.Application")
+    xl.Documents.Open(os.path.abspath("DGS-200078_NIISe_Master_Implementation_Plan_Projek_NIISe_20210117_0802_v0.3.docx"), ReadOnly=1)
+    xl.Application.Run("italicCorrection.detectLang")
 
 app = Flask(__name__)
 
